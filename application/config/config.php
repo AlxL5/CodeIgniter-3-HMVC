11593283f24ff18975faf84ee6f00466ba4f48d6
AlxL5 <alexandr.minenko@gmail.com>
2017-03-02 22:58:29 +0700

    Add hmvc

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$prototype = "http" . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "s" : "") . "://";
+$server = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+$config['base_url'] = $prototype . $server;
 
 /*
 |--------------------------------------------------------------------------
